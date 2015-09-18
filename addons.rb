4f4332f768bfd6d0939cb60406a83143f0dca773
darmes <david.armes90@gmail.com>
2015-09-17 23:50:41 -0500

    Revert "Removed deprecated 'to_yaml'"
    This reverts commit 7284ad4c2c52fd77074d0f2a8fdb5188c261533f.

--- addons.rb
+++ addons.rb
@@ -11,8 +11,7 @@
 #-------------------------------------------------------------------------------------
 class Hash
   # Replacing the to_yaml function so it'll serializes hashes sorted (by their keys)
-  #def to_yaml( opts = {} ) - deprecated
-  def encode_with( opts = {} )
+  def to_yaml( opts = {} )
     YAML::quick_emit( object_id, opts ) do |out|
       out.map( taguri, to_yaml_style ) do |map|
         sort.each do |k, v|   # <-- here's my addition (the 'sort')
